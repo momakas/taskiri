9af2de65c2f6ad593bd0385e3cdbf0d5f1ca0832
kkgtycb@yahoo.co.jp <P3G62rv2R2bA>
2022-02-28 07:58:48 +0900

    topPage作成

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Taskiri
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # エラ〜メッセージ日本語化
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
